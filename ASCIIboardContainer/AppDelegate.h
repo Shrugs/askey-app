e948ea291c093274b404330d3d46d1566deaac18
Matt Condon <mattgcondon@gmail.com>
2014-09-22 22:42:00 -0500

    Initial Commit

---
+++ ASCIIboardContainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASCIIboardContainer
+//
+//  Created by Matt Condon on 9/22/14.
+//  Copyright (c) 2014 Shrugs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASCIIboardContainer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASCIIboardContainer
+//
+//  Created by Matt Condon on 9/22/14.
+//  Copyright (c) 2014 Shrugs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
